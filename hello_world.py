2cdedb299d40e7dd8c0f165c77e7368f01874003
angel <angelmariscurrena@gmail.com>
2024-04-13 18:29:28 -0600

    Creating the python file

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+print("Hello World")
+print("This is my first time doing a CI/CD pipeline using GitHub Actions")
+print("Hope everything goes fine c:")
+print("\n ... \n \n")
+print("Just for fun, lets add a For cycle to this test file haha")
+
+for i in range(0, 10):
+    print(i)
+
+print("Le grand finale")
